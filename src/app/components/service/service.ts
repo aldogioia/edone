081a340a2f098a0b52ddfba95947592c27e1e207
aldogioia <aldogioia2002@gmail.com>
2025-12-06 00:01:20 +0100

    Terminato il sito

--- src/app/components/service/service.ts
+++ src/app/components/service/service.ts
@@ -8,7 +8,6 @@ import {Component, input} from '@angular/core';
 })
 export class Service {
   name = input.required<string>();
-  description = input.required<string>();
   price = input.required<string>();
   duration = input.required<string>();
   imgUrl = input.required<string>();
