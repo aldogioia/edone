5b272220a008803c52faaadbd1d5423a8df1d33b
aldogioia <aldogioia2002@gmail.com>
2026-02-17 22:25:25 +0100

    Ordinati i componenti tra customer e admin

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { Footer } from './components/footer/footer';
 import {HugeiconsIconComponent} from '@hugeicons/angular';
-import { Service } from './components/service/service';
-import { Rating } from './components/rating/rating';
-import { AppFeature } from './components/app-feature/app-feature';
-import { AppStores } from './components/app-stores/app-stores';
-import { Login } from './screen/login/login';
 import {ReactiveFormsModule} from '@angular/forms';
-import { Home } from './screen/home/home';
-import { ListItem } from './admin-components/list-item/list-item';
-import { CustomersPage } from './screen/customers-page/customers-page';
-import { ToolsPage } from './screen/tools-page/tools-page';
-import { AdminHome } from './screen/admin-home/admin-home';
+import {Service} from './customers/components/service/service';
+import {Footer} from './customers/components/footer/footer';
+import {Rating} from './customers/components/rating/rating';
+import {AppFeature} from './customers/components/app-feature/app-feature';
+import {AppStores} from './customers/components/app-stores/app-stores';
+import {Login} from './admin/screens/login/login';
+import {Home} from './customers/screen/home/home';
+import {ListItem} from './admin/components/list-item/list-item';
+import {CustomersPage} from './admin/customers-page/customers-page';
+import {ToolsPage} from './admin/screens/tools-page/tools-page';
+import {AdminHome} from './admin/admin-home/admin-home';
 
 @NgModule({
   declarations: [
